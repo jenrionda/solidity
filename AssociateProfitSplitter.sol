7ba6c50153e47dec0cb15a3e4a4e669e470575cc
jenrionda@gmail.com <jenrionda@gmail.com>
2020-02-12 19:37:00 -0500

    final submission

--- AssociateProfitSplitter.sol
+++ AssociateProfitSplitter.sol
@@ -1,32 +1,34 @@
 pragma solidity ^0.5.0;
-
-// lvl 1: equal split
-contract AssociateProfitSplitter {
-    // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
-
+// lvl 2: tiered split
+contract TieredProfitSplitter {
+    address payable employee_one; // ceo
+    address payable employee_two; // cto
+    address payable employee_three; // bob
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
         employee_two = _two;
         employee_three = _three;
     }
-
+    // Should always return 0!
     function balance() public view returns(uint) {
         return address(this).balance;
     }
-
     function deposit() public payable {
-        // @TODO: Split `msg.value` into three
-        uint amount = ; // Your code here!
-
-        // @TODO: Transfer the amount to each employee
-        // Your code here!
-
-        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
+        uint points = msg.value / 100;
+        uint total;
+        uint amount;
+        amount = points * 60; // "60%" distribution
+        total += amount;
+        employee_one.transfer(amount);
+        amount = points * 25; // "25%" distribution
+        total += amount;
+        employee_two.transfer(amount);
+        amount = points * 15; // "15%" distribution
+        total += amount;
+        employee_three.transfer(amount);
+        employee_one.transfer(msg.value - total); // ceo gets the remaining wei
     }
-
     function() external payable {
-        // @TODO: Enforce that the `deposit` function is called in the fallback function!
-        // Your code here!
+        deposit();
     }
 }
